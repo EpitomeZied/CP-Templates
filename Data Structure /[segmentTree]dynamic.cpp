9cd6bb55862431e4abbdf94b42ae8f4e26117239
Mahmoud Abo-Zied <116457362+EpitomeZied@users.noreply.github.com>
2025-12-28 22:38:28 +0200

    Comment out Solve function in segmentTree dynamic.cpp
    Commented out the Solve function that handles input and queries for the segment tree.

--- Data Structure /[segmentTree]dynamic.cpp
+++ Data Structure /[segmentTree]dynamic.cpp
@@ -36,3 +36,30 @@ int query(int qs,int qe, node *&cur,int ns = 1,int ne = 1e9) {
     int m = ns + (ne - ns) / 2;
     return query(qs, qe, cur->l, ns, m) + query(qs, qe, cur->r, m + 1, ne);
 }
+/*
+auto Solve(const int &n) -> void {
+    int q;
+    cin >> q;
+    node *root = EMPTY;
+    vi a(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        cin >> a[i];
+        add(a[i], 1, root);
+    }
+
+    for (int i = 0; i < q; ++i) {
+        char ty;
+        cin >> ty;
+        if (ty == '!') {
+            int idx, new_salary;
+            cin >> idx >> new_salary;
+            add(a[idx], -1, root);
+            a[idx] = new_salary;
+            add(a[idx], 1, root);
+        }else {
+            int s , e ; cin >> s >> e ;
+            cout << query(s , e , root) << endl;
+        }
+    }
+}
+*/
